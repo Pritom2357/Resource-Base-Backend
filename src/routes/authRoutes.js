17f0ef41689e3fcac9a28a5fe5a7143b14579b55
Pritom2357 <pritombiswas9999@gmail.com>
2025-04-05 17:34:17 +0600

    added password change method and fixed the error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,7 +11,7 @@ router.post('/login', authController.login);
 router.post('/token', authController.refreshToken);
 router.post('/logout', authController.logout);
 router.post('/logout/all', authenticateToken, authController.logoutAll);
-router.post('/change-password', authController, authController.changePassword);
+router.post('/change-password', authenticateToken, authController.changePassword);
 
 router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 router.get('/google/callback', 
